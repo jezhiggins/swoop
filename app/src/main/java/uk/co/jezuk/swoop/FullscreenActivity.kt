9f6b5b6d2320041ef96b364ffbad10a884294972
Jez Higgins <jez@jezuk.co.uk>
2020-05-29 13:25:41 +0100

    Swap the TextView for a SurfaceView. This is what I'll draw on. I hope :)

--- app/src/main/java/uk/co/jezuk/swoop/FullscreenActivity.kt
+++ app/src/main/java/uk/co/jezuk/swoop/FullscreenActivity.kt
@@ -5,17 +5,16 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.view.MotionEvent
+import android.view.SurfaceView
 import android.view.View
-import android.widget.Button
 import android.widget.LinearLayout
-import android.widget.TextView
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 class FullscreenActivity : AppCompatActivity() {
-    private lateinit var fullscreenContent: TextView
+    private lateinit var fullscreenContent: SurfaceView
     private lateinit var fullscreenContentControls: LinearLayout
     private val hideHandler = Handler()
 
